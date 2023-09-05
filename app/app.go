3f965baf7c1a8816dd6fa82382ef9c4bb1b65bda
Thiago Lima <thiagodiaslima10@gmail.com>
2023-09-05 00:58:04 -0300

    Criando função para buscar ip de provedores e passando provedor default

--- app/app.go
+++ app/app.go
@@ -1,12 +1,47 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Returns the command line application ready to run
+	"github.com/urfave/cli"
+)
+
+const USAGE string = "Search For IPs And Server Names on The Internet"
+
+// Returns the command line application ready to run
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command Line Application"
-	app.Usage = "Search For IPs And Server Names on The Internet"
+	app.Usage = USAGE
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: USAGE,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "instagram.com",
+				},
+			},
+			Action: searchIps,
+		},
+	}
 
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
